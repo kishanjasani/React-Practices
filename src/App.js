18fa028c59340330ab3d176188d6d0e0eae27031
Kishan Jasani <kishanjasani007@yahoo.in>
2019-07-30 17:37:25 +0530

    Add JSX to App.js file

--- src/App.js
+++ src/App.js
@@ -4,25 +4,32 @@ import Pet from "./Pet";
 
 // throw new Error('lol');
 const App = () => {
-  return React.createElement("div", { id: "somthing-important" }, [
-    React.createElement("h1", {}, "React Learning"),
-    React.createElement(Pet, {
-      name: "Lalu",
-      animal: "Pintu",
-      breed: "Havanes"
-    }),
-
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cocktiel"
-    }),
-
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed"
-    })
-  ]);
+  // return React.createElement("div", { id: "somthing-important" }, [
+  //   React.createElement("h1", {}, "React Learning"),
+  //   React.createElement(Pet, {
+  //     name: "Lalu",
+  //     animal: "Pintu",
+  //     breed: "Havanes"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Pepper",
+  //     animal: "Bird",
+  //     breed: "Cocktiel"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Doink",
+  //     animal: "Cat",
+  //     breed: "Mixed"
+  //   })
+  // ]);
+  return (
+    <div>
+      <h1 id="somthing-important">React Learning</h1>
+      <Pet name="Lalu" animal="Pintu" breed="Havanes" />
+      <Pet name="Pepper" animal="Bird" breed="Cocktails" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
+// render(React.createElement(App), document.getElementById("root"));
