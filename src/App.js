c67861f2da4a2ffc6a83d53275bbce88a99b3237
Kishan Jasani <kishanjasani007@yahoo.in>
2019-07-30 16:28:50 +0530

    Add simple react example

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+// throw new Error('lol');
+const App = () => {
+  return React.createElement("div", { id: "somthing-important" }, [
+    React.createElement("h1", {}, "React Learning"),
+    React.createElement(Pet, {
+      name: "Lalu",
+      animal: "Pintu",
+      breed: "Havanes"
+    }),
+
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cocktiel"
+    }),
+
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mixed"
+    })
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
