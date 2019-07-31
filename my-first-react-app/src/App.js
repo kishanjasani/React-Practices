7c9c0c2200be790dd5c101f94f8f6d395d430333
Kishan Jasani <kishanjasani007@yahoo.in>
2019-07-31 13:19:44 +0530

    Add counter and update object functionality in app

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hii, It's my First React App</h1>
-      <Person name="Kishan" age="23" />
-      <Person name="Manav" age="21">Hobbies: Cricket, Reading Books</Person>
-      <Person name="Meet" age="16" />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    persons: [
+      {name: "Kishan", age: 23},
+      {name: "Manav", age: 21},
+      {name: "Meet", age: 16}
+    ],
+    count: 0
+  }
+
+  switchNameHandler = () => {
+    this.setState( {
+      persons: [
+        {name: "Kishan Jasani", age: 22},
+        {name: "Manav", age: 21},
+        {name: "Meet Jasani", age: 15}
+      ]
+    })
+  }
+
+  counterHandler = () => {
+    //console.log(this.state.count);
+    this.setState({count: this.state.count + 1});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hii, It's my First React App</h1>
+        <button onClick={this.counterHandler}>Counter</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <h2>{this.state.count + ""}</h2>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Cricket, Reading Books</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- my-first-react-app/src/Person/Person.js
+++ my-first-react-app/src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const person = (props) => {
-    console.log(props);
+    //console.log(props);
     const {name, age, children} = props;
     return (
         <div> I'm a {name} and {age} years old! {children !== undefined ? ' and ' + children : ''} </div>
