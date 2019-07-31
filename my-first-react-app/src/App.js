5a3e06ac260e3b8ddc749ba25c9c51deb7a77722
Kishan Jasani <kishanjasani007@yahoo.in>
2019-07-31 11:28:40 +0530

    Add simple functional component

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
       <h1>Hii, It's my First React App</h1>
+      <Person />
     </div>
   );
 }
---
+++ my-first-react-app/src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const person = () => {
+    return (
+        <div> I'm a Person </div>
+    );
+}
+
+export default person;
