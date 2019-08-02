12532e34c018cee3ef55300fe9a9959bdbc26d61
Kishan Jasani <kishanjasani007@yahoo.in>
2019-08-02 15:24:56 +0530

    Add PropTypes to component

--- my-first-react-app/src/components/Persons/Person/Person.js
+++ my-first-react-app/src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import classes from './Person.css';
+import PropTypes from 'prop-types';
 
 class Person extends Component {
     render() {
@@ -15,5 +16,12 @@ class Person extends Component {
         );
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default Person;
--- my-first-react-app/src/containers/App.js
+++ my-first-react-app/src/containers/App.js
@@ -20,7 +20,8 @@ class App extends Component {
     ],
     count: 0,
     showPerson: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   }
 
   static getDerivedStateFromProps = (props, state) => {
@@ -55,7 +56,12 @@ class App extends Component {
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
-    this.setState({persons: persons});
+    this.setState( (prevState, props) => {
+      return {
+        persons: persons,
+        changeCounter: prevState.changeCounter + 1
+      }
+    });
   }
 
   deletePersonHandler = (index) => {
