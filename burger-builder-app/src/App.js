a53f6bc2fa7f99ac57f68292cd69bd804a24dc46
Kishan Jasani <kishanjasani007@yahoo.in>
2019-08-03 17:54:47 +0530

    Add burger with hardcoded component

--- burger-builder-app/src/App.js
+++ burger-builder-app/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Layout>
+            <BurgerBuilder />
+          </Layout>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
