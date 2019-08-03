768b6bad934785149b61cc019f7ea053f23167e8
Kishan Jasani <kishanjasani007@yahoo.in>
2019-08-03 19:45:52 +0530

    Add burger ingredient functionality dynamicaly with buttons

--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    meat:  1.3,
+    cheese: 0.4,
+    bacon: 0.7
+}
 class BurgerBuilder extends Component {
 
     state = {
@@ -10,14 +17,43 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updateIngredients = {
+            ...this.state.ingredients
+        };
+        updateIngredients[type] = updatedCount;
+
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updateIngredients});
+    }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount - 1;
+        const updateIngredients = {
+            ...this.state.ingredients
+        };
+        updateIngredients[type] = updatedCount;
+
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updateIngredients});
     }
 
     render() {
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls ingredientRemoved={this.removeIngredientHandler} ingredientAdded={this.addIngredientHandler} />
             </Aux>
         );
     }
