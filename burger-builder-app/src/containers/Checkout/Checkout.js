b9eeb27155ad4d8ae8c8e823fe60198dc2179f82
Kishan Jasani <kishanjasani007@yahoo.in>
2019-08-11 11:25:53 +0530

    Extract queryparameter data

--- burger-builder-app/src/containers/Checkout/Checkout.js
+++ burger-builder-app/src/containers/Checkout/Checkout.js
@@ -10,6 +10,14 @@ class Checkout extends Component {
             cheese: 1
         }
     }
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for (let param of query.entries()) {
+            ingredients[param[0]] = +param[1];
+        }
+        this.setState({ingredients: ingredients});
+    }
     checkoutContinuedHandler = () => {
         this.props.history.replace('/checkout/contact-data');
     }
